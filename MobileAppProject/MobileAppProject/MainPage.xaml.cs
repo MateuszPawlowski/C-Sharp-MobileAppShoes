a15ba6645653a04d463b0d4db2935a8c969c7368
MATEUSZ PAWLOWSKI <G00361162@gmit.ie>
2018-12-12 15:34:56 +0000

    Importing project

---
+++ MobileAppProject/MobileAppProject/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MobileAppProject
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+     
+        }
+
+        private async void AboutUs_Click(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AboutUs());
+        }
+
+        private async void Login_Click(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Login());
+        }
+
+        private async void Shop_Click(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Shop());
+        }
+
+    }
+}
