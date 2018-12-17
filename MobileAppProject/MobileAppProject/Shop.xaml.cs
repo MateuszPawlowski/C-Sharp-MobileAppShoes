f3737e9927f24a7c75b183edb690257e02ad4481
MATEUSZ PAWLOWSKI <G00361162@gmit.ie>
2018-12-17 13:19:53 +0000

    Commit Project

--- MobileAppProject/MobileAppProject/Shop.xaml.cs
+++ MobileAppProject/MobileAppProject/Shop.xaml.cs
@@ -21,12 +21,54 @@ namespace MobileAppProject
         {
             await Navigation.PushAsync(new Adidas());
         }
+        private async void Adidas_Click(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Adidas());
+        }
 
         private async void Puma_Tap(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new Puma());
         }
+        private async void Puma_Click(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Puma());
+        }
 
- 
+        private async void Nike_Tap(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Nike());
+        }
+        private async void Nike_Click(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Nike());
+        }
+
+        private async void Converse_Tap(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Converse());
+        }
+        private async void Converse_Click(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Converse());
+        }
+
+        private async void Reebok_Tap(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Reebok());
+        }
+        private async void Reebok_Click(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Reebok());
+        }
+
+        private async void Timberland_Tap(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Timberland());
+        }
+        private async void Timberland_Click(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Timberland());
+        }
     }
 }
